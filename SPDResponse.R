a9c623f07dc476f1ce064f7d3f0b57b6d1e47280
randhyllcho <randhyllcho@gmail.com>
2017-02-24 13:26:51 -0800

    More EDA. Added a map and some filtering

--- SPDResponse.R
+++ SPDResponse.R
@@ -12,6 +12,14 @@ ggplot(responses, aes(as.factor(Event.Clearance.Group))) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
   labs(x = "Event Clearence Group")
 
+responses %>% filter(Event.Clearance.Group=="SUSPICIOUS CIRCUMSTANCES")
+
+ggplot(responses %>% filter(Initial.Type.Description == "CHILD - LURING"), aes(Longitude, Latitude)) +
+  geom_point()
+
+ggmap(seaMap) +
+  geom_point(data = responses %>% filter(Initial.Type.Description == "CHILD - LURING"), aes(Longitude, Latitude))
+
 gatherings <- responses %>% filter(Event.Clearance.Group == "PUBLIC GATHERINGS") 
 gatherings <- gatherings %>% separate(At.Scene.Time, c("Year", "Month", "Day", "Hour", "minute", "Second"))
 dat <- gatherings %>% filter(Event.Clearance.Description == "DEMONSTRATION MANAGEMENT (Control tactics used)") %>% group_by(Year) 
@@ -20,7 +28,7 @@ ggplot(gatherings, aes(Longitude, Latitude, col = Year)) +
 
 seattle <- geocode("seattle")
 
-seaMap <- get_map(seattle, zoom = 12)
+seaMap <- get_map(seattle, zoom = 10)
 ggmap(seaMap) +
   geom_density2d(data = gatherings, aes(Longitude, Latitude, col = Year), size = 2, alpha = 0.7)
 
