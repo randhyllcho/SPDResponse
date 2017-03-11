a7626f08bf3e6acc6f3041db2f42b8323a7e7aec
randhyllcho <randhyllcho@gmail.com>
2017-03-10 16:21:07 -0800

    added new plot for aesthetics only

--- SPDResponse.R
+++ SPDResponse.R
@@ -32,3 +32,18 @@ seaMap <- get_map(seattle, zoom = 10)
 ggmap(seaMap) +
   geom_density2d(data = gatherings, aes(Longitude, Latitude, col = Year), size = 2, alpha = 0.7)
 
+ggplot(responses %>% filter(Initial.Type.Subgroup == "MOTOR VEHICLE COLLISION INVESTIGATION", At.Scene.Time <= "2016-12-31"), aes(Longitude, Latitude)) +
+  geom_point(alpha = 0.1, col = "#331A00") +
+  theme_classic() +
+  theme(axis.line = element_blank(), 
+        axis.text = element_blank(), 
+        axis.ticks = element_blank(), 
+        axis.title = element_blank(), 
+        aspect.ratio = 4/3,
+        legend.text = element_text(size = 7),
+        legend.position = "bottom", 
+        strip.background = element_blank(),
+        strip.text = element_blank()) 
+
+
+
